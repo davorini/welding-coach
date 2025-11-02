c23222feb62072e087e12b3bc88adaa77aee051f
davorini <73196737+davorini@users.noreply.github.com>
2025-11-02 20:24:16 +0100

    Remove Page component from page.tsx

--- page.tsx
+++ page.tsx
@@ -1,19 +1,1 @@
-```tsx
 
-export default function Page() {
-
-  return (
-
-    <div className="min-h-screen p-8">
-
-      <h1 className="text-4xl font-bold">Varilec Asistent</h1>
-
-      <p className="mt-4">Aplikacija deluje!</p>
-
-    </div>
-
-  )
-
-}
-
-```
